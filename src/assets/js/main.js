65dad41a4148cfb23110f69fb98f23fb9f9f965d
Chad <chad@summitathletics.com>
2023-04-24 11:01:30 -0400

    Added comments in main.js and header.kit

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,13 +5,9 @@ ie10();
 import modernizrInit from "./base/modernizr-webp.js";
 modernizrInit();
 
-// Bootstrap - not using full js bundle, see below
-//  "../../../node_modules/bootstrap/dist/js/bootstrap.js";
-
+/* ========================= NPM ======================== */
 import AOS from "aos";
 AOS.init();
-
-// import Popper from "popper.js";
 
 /* ===================== COMPONENTS ===================== */
 import currentYear from "./components/current-year.js";
